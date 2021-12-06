0fceebda8de9e01bff6dc819fd158c7a79097624
Matej Kuka <mate1778@easv365.dk>
2021-12-06 21:13:18 +0100

    Implemented BE.Song

--- src/Mytunes/BE/Song.java
+++ src/Mytunes/BE/Song.java
@@ -1,8 +1,31 @@
 package Mytunes.BE;
 
+
+
 public class Song {
     String title;
     String artist;
     String category;
-    
+
+    private static int minute;
+    private static int second;
+
+    public Song(String title, String artist){
+        this.minute = (int)((System.currentTimeMillis() / 1000) /60 ) % 60;
+        this.second =(int)(System.currentTimeMillis() /1000) % 60;
+        this.artist = artist;
+        this.title = title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void TimeToString(){
+        String.format(this.minute +":"+ this.second);
+    }
 }
