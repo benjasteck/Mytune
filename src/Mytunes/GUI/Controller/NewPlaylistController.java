e150b193904de202afb7bd91c43016c2f821fb1a
Matej Kuka <mate1778@easv365.dk>
2021-12-08 22:39:06 +0100

    created Views and controllers for adding new playlist and song

---
+++ src/Mytunes/GUI/Controller/NewPlaylistController.java
@@ -0,0 +1,13 @@
+package Mytunes.GUI.Controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class NewPlaylistController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
---
+++ src/Mytunes/GUI/Controller/NewSongController.java
@@ -0,0 +1,14 @@
+package Mytunes.GUI.Controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class NewSongController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
