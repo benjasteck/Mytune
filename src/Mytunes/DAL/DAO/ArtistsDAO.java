ccbe69c1655e135b5eda227235e9edecd8ac2272
benjasteck <90683212+benjasteck@users.noreply.github.com>
2021-12-10 17:11:44 +0100

    Update ArtistsDAO.java

--- src/Mytunes/DAL/DAO/ArtistsDAO.java
+++ src/Mytunes/DAL/DAO/ArtistsDAO.java
@@ -3,10 +3,7 @@ package Mytunes.DAL.DAO;
 import Mytunes.BLL.ArtistBLL;
 import Mytunes.DAL.database.DbConnector;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class ArtistsDAO {
     DbConnector databaseConnector;
@@ -17,16 +14,51 @@ public class ArtistsDAO {
 
     public int createArtist(String name) throws SQLException {
         //todo when artist is searched for or submitted, it should return the id of that artist. if this one does not exist it should create an id and return it.
-        return 1;
+        int artistid = 0;
+        String sql = "SELECT * FROM artists WHERE Name = ?";
+        try (Connection connection = databaseConnector.getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, name);
+            preparedStatement.execute();
+            ResultSet resultSet = preparedStatement.getResultSet();
+            while (resultSet.next()) {
+                artistid = resultSet.getInt("artistId");
+                return artistid;
+            }
+            String sql1 = "INSERT INTO artists VALUES (?)";
+            PreparedStatement preparedStatement1 = connection.prepareStatement(sql1, Statement.RETURN_GENERATED_KEYS);
+            preparedStatement1.setString(1,name);
+            preparedStatement1.executeUpdate();
+            ResultSet resultSet1 = preparedStatement1.getGeneratedKeys();
+            while (resultSet1.next()) {
+                artistid = resultSet.getInt(1);
+            }
+
+        }
+        return artistid;
     }
 
     public void deleteArtist(int id) throws SQLException {
     //todo making it able to delete from artist cloumn.
+        String sql = "DELETE FROM artists WHERE Id = ?";
+        try (Connection connection = databaseConnector.getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, id);
+            preparedStatement.executeUpdate();
+        }
     }
 
 
     public void updateArtist(int id, String name) throws SQLException {
     //todo abiltity to update the artist
+        String sql = "UPDATE TABLE SET Name=? WHERE Id = ?";
+        try (Connection connection = databaseConnector.getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, name);
+            preparedStatement.setInt(2, id);
+            preparedStatement.executeUpdate();
+
+        }
     }
 
     public ArtistBLL getArtistById(int artistId) throws SQLException {
