ef6e80778179fe52d0deca1773aed77d74d1ca9e
Matej Kuka <mate1778@easv365.dk>
2021-12-19 00:29:11 +0100

    mini update in SongBLL

--- src/Mytunes/BLL/old/SongBLL.java
+++ src/Mytunes/BLL/old/SongBLL.java
@@ -2,6 +2,7 @@ package Mytunes.BLL.old;
 
 import Mytunes.BE.Song;
 import Mytunes.DAL.DAO.SongDAO;
+import Mytunes.DAL.fakeDAO.Fake_SongDAO;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,19 +10,18 @@ import java.util.List;
 public class SongBLL  {
 
     SongDAO songdatabase;
-    public List<Song> songListTest; // just test list of all Songs
+    Fake_SongDAO temporarilySongDatabase;
 
     public SongBLL(int id, String title, String artist, String category, String time, String filepath) {
     }
 
     public void createSong (Song song){
         //return songdatabase.createSong(song);
-        songListTest.add(song);
+        temporarilySongDatabase.getAllSongs().add(song);
     } // this method should create a new song
   // TODO need to alter this
 
     public SongBLL (){
-        songListTest = new ArrayList<>();
 
     } // temporarily like this
 
@@ -29,19 +29,19 @@ public class SongBLL  {
 
 
     public void deleteSongByName(String name){
-        songListTest.removeIf(song -> song.getTitle().equals(name));
+        temporarilySongDatabase.getAllSongs().removeIf(song -> song.getTitle().equals(name));
     } // this method remove the song by name
 
 
     public List<Song> getAllSongs() {
         //return songdatabase.getAllSongs();
-        return songListTest; // temporarily like this
+        return temporarilySongDatabase.getAllSongs(); // temporarily like this
         // TODO This does not work - problems with the method in SongDAO
     }
 
     public void printNumberOfSongs(){
-        System.out.println(songListTest.size());
-        for (Song song:songListTest) {
+        System.out.println(temporarilySongDatabase.getAllSongs().size());
+        for (Song song:temporarilySongDatabase.getAllSongs()) {
             System.out.println(song);
         }
         System.out.println();
